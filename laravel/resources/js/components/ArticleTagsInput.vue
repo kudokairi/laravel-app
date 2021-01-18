e772e4c782822b29ad225770f481ef97a559ac94
工藤海里 <49087795+kudokairi@users.noreply.github.com>
2021-01-18 17:57:23 +0900

    タグ機能実装

--- laravel/resources/js/components/ArticleTagsInput.vue
+++ laravel/resources/js/components/ArticleTagsInput.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
+    <input
+      type="hidden"
+      name="tags"
+      :value="tagsJson"
+    >
     <vue-tags-input
       v-model="tag"
       :tags="tags"
+      placeholder="タグを5個まで入力できます"
       :autocomplete-items="filteredItems"
+      :add-on-key="[13, 32]"
       @tags-changed="newTags => tags = newTags"
     />
   </div>
@@ -16,21 +23,20 @@ export default {
   components: {
     VueTagsInput,
   },
+  props: {
+    initialTags: {
+      type: Array,
+      default: [],
+    },
+    autocompleteItems: {
+      type: Array,
+      default: [],
+    },
+  },
   data() {
     return {
       tag: '',
-      tags: [],
-      autocompleteItems: [{
-        text: 'Spain',
-      }, {
-        text: 'France',
-      }, {
-        text: 'USA',
-      }, {
-        text: 'Germany',
-      }, {
-        text: 'China',
-      }],
+      tags: this.initialTags,
     };
   },
   computed: {
@@ -38,6 +44,9 @@ export default {
       return this.autocompleteItems.filter(i => {
         return i.text.toLowerCase().indexOf(this.tag.toLowerCase()) !== -1;
       });
+    },
+    tagsJson() {
+      return JSON.stringify(this.tags)
     },
   },
 };
@@ -55,5 +64,9 @@ export default {
     margin-right: 4px;
     border-radius: 0px;
     font-size: 13px;
+  }
+
+  .vue-tags-input .ti-tag::before {
+    content: "#";
   }
 </style>
