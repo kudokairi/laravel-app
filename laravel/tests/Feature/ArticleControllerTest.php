b73b1d811634c9dad032d9bc7b196e5c547dfbd7
工藤海里 <49087795+kudokairi@users.noreply.github.com>
2021-02-05 12:32:27 +0900

    テスト内容の一時退避

--- laravel/tests/Feature/ArticleControllerTest.php
+++ laravel/tests/Feature/ArticleControllerTest.php
@@ -7,23 +7,4 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ArticleControllerTest extends TestCase
-{
-    use RefreshDatabase;
-
-    public function testIndex()
-    {
-        $response = $this->get(route('articles.index'));
-
-        $response->assertStatus(200)
-            ->assertViewIs('articles.index');
-    }
-
-    public function testGuestCreate()
-    {
-        $response = $this->get(route('articles.create'));
-
-        $response->assertRedirect(route('login'));
-    }
-
-}
+class ArticleControllerTest extends TestCase{}
